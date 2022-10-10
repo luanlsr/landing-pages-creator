6138474c30008852847d819f8d7857da164177a7
luanfswd <luanfswd@gmail.com>
2022-10-10 14:20:09 -0300

    start project

---
+++ src/template/App/index.jsx
@@ -0,0 +1,13 @@
+import { Wrapper } from './styles';
+
+function Home() {
+  return (
+    <div className="Home">
+      <Wrapper background="#ff43fc">
+        <h1>Hello</h1>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Home;
